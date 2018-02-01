07c8a3c0ca450f09696376d7113df7c24aceb2ae
Arun .A.R <arunnadar1994@gmail.com>
2018-02-01 11:20:00 +0530

    tale

--- controller_4.lua
+++ controller_4.lua
@@ -12,3 +12,16 @@ end
 function destroy()
 end
 --------------------------------------------------------------------------------
+----------------------------------addr fn---------------------------------------
+-----------A hash function that decides the address of the robot----------------
+--------------------------------------------------------------------------------
+function addr(s)
+    i = 0
+    id = 0
+    for c in s:gmatch"." do
+        id = id + (string.byte(c) * math.pow(2 , i))
+        i = i + 1
+    end
+    id = math.fmod(id,251) + 1
+    return id
+end
