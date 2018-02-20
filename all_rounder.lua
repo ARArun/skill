2942b1854f1b135ec9584d7ab3dd35102e3cda34
Arun .A.R <arunnadar1994@gmail.com>
2018-02-20 14:06:02 +0530

    tale

--- all_rounder.lua
+++ all_rounder.lua
@@ -38,3 +38,30 @@ function addr(s)
     return id
 end
 --------------------------------------------------------------------------------
+function search()
+    bat_cur = bat_cur - 10
+    sensingLeft =     robot.proximity[3].value +
+                      robot.proximity[4].value +
+                      robot.proximity[5].value +
+                      robot.proximity[6].value +
+                      robot.proximity[2].value +
+                      robot.proximity[1].value
+
+    sensingRight =    robot.proximity[19].value +
+                      robot.proximity[20].value +
+                      robot.proximity[21].value +
+                      robot.proximity[22].value +
+                      robot.proximity[24].value +
+                      robot.proximity[23].value
+    if bat_cur <= (30/100) * bat_total then
+        state = "to_charge"
+    end
+    if sensingLeft ~= 0 then
+          robot.wheels.set_velocity(7,3)
+    elseif sensingRight ~= 0 then
+          robot.wheels.set_velocity(3,7)
+    else
+          robot.wheels.set_velocity(10,10)
+    end
+end
+--------------------------------------------------------------------------------
