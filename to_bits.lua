e79799534c4707aa7006514dd1b8b0722250578d
Arun .A.R <arunnadar1994@gmail.com>
2018-02-21 10:27:28 +0530

    borders_and_message_complexity

---
+++ to_bits.lua
@@ -0,0 +1,13 @@
+function toBits(num,bits)
+    -- returns a table of bits, most significant first.
+    bits = bits or math.max(1, select(2, math.frexp(num)))
+    local t = {} -- will contain the bits
+    for b = bits, 1, -1 do
+        t[b] = math.fmod(num, 2)
+        num = math.floor((num - t[b]) / 2)
+    end
+    return t
+end
+
+ans = toBits(11,8)
+print(ans[1],ans[2],ans[3],ans[4],ans[5],ans[6],ans[7],ans[8])
